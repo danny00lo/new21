b95bc7a231472aa9a5a12d97b016a7ebaf498c32
danny00lo <860222083@qq.com>
2017-11-29 09:16:30 +0800

    1. change the api 2. add the new params for index page

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,10 +1,7 @@
 export const config = {
   url:'/user',
   method:'post',
-  // baseURL:'http://192.168.1.37:8087/cloud2.activity.api',
-  // baseURL:'http://random.haiguime.com/cloud2.activity.api',
-  baseURL:'http://randomtest.haiguime.com/cloud2.activity.api',
-  // baseURL:'https://zytest1.haiguime.com/',
+  baseURL:'http://115.28.190.212/zhongzi/v2.php/',
 
   transformRequest: [function (data) {
     // Do whatever you want to transform the data
@@ -22,10 +19,7 @@ export const config = {
 export const config2 = {
   url:'/user',
   method:'get',
-  // baseURL:'http://192.168.1.37:8087/cloud2.activity.api',
-  // baseURL:'http://random.haiguime.com/cloud2.activity.api',
-  baseURL:'http://randomtest.haiguime.com/cloud2.activity.api',
-  // baseURL:'https://zytest1.haiguime.com/',
+  baseURL:'http://115.28.190.212/zhongzi/v2.php/',
 
   transformRequest: [function (data) {
     // Do whatever you want to transform the data
@@ -41,7 +35,7 @@ export const config2 = {
 };
 
 //常量
-export const axiosUrl = 'http://api.dkey.me/zhongzi/v2.php/';  //测试服务器
+export const axiosUrl = 'http://115.28.190.212/zhongzi/v2.php/';  //测试服务器
 // export const axiosUrl = 'http://192.168.1.37:8087/'; //本地测试服务器
 
 export const downloadURL = "http://www.idothing.com/download.html";
